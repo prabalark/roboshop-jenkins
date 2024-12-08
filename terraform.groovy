b03e7958867f81fadabc0a582b02ee9ff071a43f
prabala <radhakrishnaprabala8@gmail.com>
2024-12-08 21:34:17 +0530

    jenkinsfile vr1

---
+++ terraform.groovy
@@ -0,0 +1,30 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'terraform-workstation'
+            }
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+        stages {
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
