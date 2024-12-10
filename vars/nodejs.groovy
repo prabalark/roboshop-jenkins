fcf8d82d9497f7e7c615012735f773da0bb99a77
prabala <radhakrishnaprabala8@gmail.com>
2024-12-10 20:45:35 +0530

    nodejs.groovy vr11

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,6 +8,7 @@ def call() {
         }
 
         stages {
+
             stage('Code Quality') {
                 steps {
                     sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
