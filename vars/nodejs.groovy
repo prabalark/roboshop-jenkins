50ad15677989b248729b8a0efe03b1c104c1346a
prabala <radhakrishnaprabala8@gmail.com>
2024-12-10 21:50:44 +0530

    nodejs.groovy vr11

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,28 +11,28 @@ def call() {
 
           stage('Code Quality') {
               steps {
-                  sh 'ls -l'
-                  sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
-                  //sh 'echo Code Quality'
+                sh 'ls -l'
+                sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                //sh 'echo Code Quality'
               }
           }
 
           stage('Unit Test Cases') {
               steps {
-                  sh 'echo Unit tests'
-                  //sh 'npm test'
+                sh 'echo Unit tests'
+                //sh 'npm test'
               }
           }
 
           stage('CheckMarx SAST Scan') {
               steps {
-                  sh 'echo Checkmarx Scan'
+                sh 'echo Checkmarx Scan'
               }
           }
 
           stage('CheckMarx SCA Scan') {
               steps {
-                  sh 'echo Checkmarx SCA Scan'
+                sh 'echo Checkmarx SCA Scan'
               }
           }
 
