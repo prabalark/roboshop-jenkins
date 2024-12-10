3e1363e4af61f3dc846d13725cddf954d8921ee9
prabala <radhakrishnaprabala8@gmail.com>
2024-12-10 21:25:57 +0530

    nodejs.groovy vr11

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,50 +1,49 @@
 def call() {
   pipeline {
 
-        agent {
-            node {
-                label 'terraform-workstation'
-            }
-        }
+     agent {
+         node {
+             label 'terraform-workstation'
+         }
+     }
 
-        stages {
+      stages {
 
-            stage('Code Quality') {
-                steps {
-                    sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
-                    //sh 'echo Code Quality'
-                }
-            }
+          stage('Code Quality') {
+              steps {
+                  sh 'ls -l'
+                  sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                  //sh 'echo Code Quality'
+              }
+          }
 
-            stage('Unit Test Cases') {
-                steps {
-                    sh 'echo Unit tests'
-                    //sh 'npm test'
-                }
-            }
+          stage('Unit Test Cases') {
+              steps {
+                  sh 'echo Unit tests'
+                  //sh 'npm test'
+              }
+          }
 
-            stage('CheckMarx SAST Scan') {
-                steps {
-                    sh 'echo Checkmarx Scan'
-                }
-            }
+          stage('CheckMarx SAST Scan') {
+              steps {
+                  sh 'echo Checkmarx Scan'
+              }
+          }
 
-            stage('CheckMarx SCA Scan') {
-                steps {
-                    sh 'echo Checkmarx SCA Scan'
-                }
-            }
+          stage('CheckMarx SCA Scan') {
+              steps {
+                  sh 'echo Checkmarx SCA Scan'
+              }
+          }
 
-        }
+      }
 
-        post {
-            always {
-                cleanWs()
-            }
-        }
+      post {
+          always {
+              cleanWs()
+          }
+      }
 
   }
-
 
 }
