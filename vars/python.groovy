a89729f43353d3b9be81cb6a00e5ac750f98b790
prabala <radhakrishnaprabala8@gmail.com>
2024-12-15 10:12:04 +0530

    python.grrovy vr1

---
+++ vars/python.groovy
@@ -0,0 +1,42 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'terraform-workstation'
+            }
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+                    sh 'ls -l'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                    //sh 'npm test'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
+            }
+
+        }
+
+    }
+
+}
