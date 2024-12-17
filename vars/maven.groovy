89448395c139580a713218271d10821e8033ef72
prabala <radhakrishnaprabala8@gmail.com>
2024-12-17 10:09:53 +0530

    nodejs.groovy vr1

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -18,8 +18,9 @@ def call() {
 
             stage('Code Quality') {
                 steps {
-                    sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                    //sh 'ls -l'
+                    //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                    sh 'echo Code Quality'
                 }
             }
 
