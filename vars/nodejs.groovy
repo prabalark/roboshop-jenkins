6d1f031039a8b49d679da28d1069aecb472b2c30
prabala <radhakrishnaprabala8@gmail.com>
2024-12-19 21:32:40 +0530

    nodejs.groovy vr1

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -45,7 +45,7 @@ def call() {
                   sh 'npm install'
                   sh 'echo $TAG_NAME >VERSION'
                   sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
-                  sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://18.204.205.16:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                  sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://54.227.172.218:8081/repository/${component}/${component}-${TAG_NAME}.zip'
               }
           }
 
