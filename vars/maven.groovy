3e293eda31992baeacd2ddb346c1ef073f5dbf6f
prabala <radhakrishnaprabala8@gmail.com>
2024-12-21 11:31:49 +0530

    nodejs.groovy vr1

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -50,9 +50,9 @@ def call() {
                     }
                 }
                 steps {
-                    sh 'mvn clean package ; cp target/{component}-1.0.jar {component}.jar '
+                    sh 'mvn package ; cp target/{component}-1.0.jar ${component}.jar '
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip {component}.jar VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
                     sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://54.157.254.32:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
